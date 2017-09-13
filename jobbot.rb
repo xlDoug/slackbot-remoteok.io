56fb505f4dc0716c7dc68e7bd9d3e5f8d2ccbf5c
Douglas Cunha <douglascunha@outlook.com>
2017-09-13 01:33:02 -0300

    tirei metodo

--- jobbot.rb
+++ jobbot.rb
@@ -1,19 +1,17 @@
 require 'json'
 require 'open-uri'
 
-def getjobs()
-  @data = JSON.load(open("https://remoteok.io/remote-ruby-jobs.json"))
-end
-
-jobs = getjobs()
-#puts jobs[1]['company']
+jobs = JSON.load(open("https://remoteok.io/remote-ruby-jobs.json"))
 
 jobs.each do |job|
-  puts "ID: " + job["id"]
-  puts "Data: "+ job["date"]
-  puts "Empresa: " + job["company"]
-  puts "Vaga: " + job["position"]
-  puts "Link: " + job["url"]
+  if job["position"].downcase.include?('senior'||'sr.')
+    next
+  end
+  puts "ID: "       + job["id"]
+  puts "Data: "     + job["date"]
+  puts "Empresa: "  + job["company"]
+  puts "Vaga: "     + job["position"]
+  puts "Link: "     + job["url"]
   puts " =================================== "
 end
 
